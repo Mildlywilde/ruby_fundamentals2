13e4f7a977faf15c3d2e739e1e4b41f7076714be
Jack WIlde <jackwilde630@gmail.com>
2016-08-10 13:33:37 -0400

    add cohort 4

--- exercise7.rb
+++ exercise7.rb
@@ -4,6 +4,11 @@ def list_hash(hash)
   hash.each do |cohort, number|
     puts "#{cohort}: #{number} students"
   end
+  puts ""
 end
+
+list_hash(students)
+
+students[:cohort4] = 43
 
 list_hash(students)
